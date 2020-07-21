bcacfa9fbcbd4b3733d2dede3df75ce151fbea52
Narongrit Rodphroboon <narongrit.rodphroboon@allianz.com>
2020-07-21 15:59:29 +0700

    formatted document

--- src/app/models/product.ts
+++ src/app/models/product.ts
@@ -1,3 +1,3 @@
 export class Product {
-    constructor(public code: string, public name: string, public price: number, public available: boolean, public rating: number, public imageUrl: string){}
+    constructor(public code: string, public name: string, public price: number, public available: boolean, public rating: number, public imageUrl: string) { }
 }
--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {Product} from './../models/product';
+import { Product } from './../models/product';
 
 @Component({
   selector: 'app-product-list',
