043146b42d8b679189f92a13d94095c9664d9afc
Narongrit Rodphroboon <narongrit.rodphroboon@allianz.com>
2020-07-21 15:52:17 +0700

    Make a dynamic table product list

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from './../models/product';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
+  products: Product[] = [];
+
   constructor() { }
 
   ngOnInit(): void {
+    const p1 = new Product('xxx', 'Test1', 20, true, 5, 'www.blahblah.com');
+    const p2 = new Product('xxy', 'Test2', 30, false, 4, 'www.blahblah2.com');
+    const p3 = new Product('xxz', 'Test3', 50, true, 3, 'www.blahblah3.com');
+    this.products.push(p1, p2, p3);
   }
 
 }
